96ccbcc07c42f52732594f01e27509af3b5cb5d8
Katty Lazar <katy.lazar05@gmail.com>
2022-01-13 15:03:13 +0200

    finished presentation part on landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Presentation from './Presentation'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Presentation />
+        </div>
+    )
+}
+
+export default LandingPage
