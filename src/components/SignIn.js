319a96bc8f985cf48d8e546e63d493233b456d0e
Katty Lazar <katy.lazar05@gmail.com>
2022-01-15 22:38:57 +0200

    wrote media queries for large screens & added logic to sign in buttons alignment

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const SignIn = () => {
+const SignIn = ( { buttonAlign } ) => {
     return (
-        <div className='sign-in'>
+        <div className={`sign-in ${ buttonAlign }`}>
             <Link to='/sign-in'>
                 <button>
                     SIGN IN
                 </button>
             </Link>
-
-            {/* <Link to='/login'>
-                <button className="login">
-                    LOGIN
-                </button>
-            </Link> */}
         </div>
     )
 }
