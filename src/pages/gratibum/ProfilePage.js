67ef8da58be80e0804da5153eba3ea2d59752474
Katty Lazar <katy.lazar05@gmail.com>
2022-06-04 17:23:37 +0300

    implemented photo upload on profile

--- src/pages/gratibum/ProfilePage.js
+++ src/pages/gratibum/ProfilePage.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
 import Back from '../../components/Back'
 import Logout from '../../components/gratibum/Logout';
+
+import { auth } from '../../services/firebase';
 
 import logo from '../../images/logo.png';
 
@@ -10,10 +12,49 @@ import { useTranslation } from 'react-i18next'
 import SelectLanguage from '../../components/SelectLanguage';
 
 const ProfilePage = () => {
-
     const {t} = useTranslation();
 
-    // const name = JSON.stringify(localStorage.getItem("currentUser")).name;
+    const [ profilePic, setProfilePic ] = useState( logo );
+    
+    const accountId = JSON.parse(localStorage.getItem('currentUser')).accountId;
+    const email = JSON.parse(localStorage.getItem('currentUser')).email;
+    const name = JSON.parse(localStorage.getItem('currentUser')).name;
+    let localstor = JSON.parse(localStorage.getItem("currentUser"));
+
+    // setProfilePic( localstor.photoUrl );
+
+    useEffect( () => {
+        setProfilePic( localstor.photoUrl);
+    }, [localstor.photoUrl]);
+
+    const UploadProfilePicture = async ( e ) => { 
+        e.preventDefault();
+
+        let fileInput = document.getElementById('profile-img-input').files[0];
+        
+        var formdata = new FormData();
+        formdata.append( "email", email );
+        formdata.append( "folder", "profile" );
+        formdata.append("image", fileInput, "106999602_3735056599854298_" + Math.random(100000000000000000, 199999999999999999) + "_n.jpeg");
+
+        var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+        };
+
+        await fetch("http://www.vecinulvirtual.ro/liw/api/images.php", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                let files = result.files;
+                setProfilePic(files.normal);
+                auth.currentUser.photoURL = files.normal;
+                localstor.photoUrl = files.normal;
+                localStorage.setItem("currentUser", JSON.stringify(localstor));
+                console.log(auth.currentUser.photoURL);
+            })
+            .catch(error => console.error('error', error));
+    }
 
     return (
         <div className={ window.innerWidth < 1000 ? 'profile-page profile-page-small sign-in-page sign-in-page-small' : 'profile-page profile-page-large sign-in-page sign-in-page-large'}>
@@ -29,12 +70,15 @@ const ProfilePage = () => {
             <section className="profile-page-info">
                 <div className="user-info">
                     <div className="profile-img">
-                        <img src={ logo } alt="" />
-                        <input type="file" accept="image/*" />
+                        <img src={ profilePic } alt="" />
+                        <input type="file" accept="image/jpeg" 
+                            id="profile-img-input"
+                            onChange={ UploadProfilePicture }
+                        />
                     </div>
 
                     <div className="profile-name">
-                        <h2>Name</h2>
+                        <h2>{ name }</h2>
                     </div>
 
                     <div className="gratitudes-nr">
